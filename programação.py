8669f4fb427da67511070fde7c18ca43901b22e4
Andressa <andressahidalgo.ah@gmail.com>
2024-08-28 21:57:57 -0300

    Add files via upload

---
+++ programação.py
@@ -0,0 +1,60 @@
+# n1 = int(input("digite um numero"))
+# n2 = int(input("digite um numero"))
+# # cal= n1/n2
+# print ('idade: % %s '% (n1, n2)) 
+
+
+# numero1 = 50
+# numero2 = 9 
+# mult = (numero1 / numero2)
+# resultado = int(mult/2)
+# print(mult)
+
+# numero = 5
+# numero1 = 7
+# soma = (numero +numero1) 
+# print(soma)
+
+# numero1 = 3 
+# numero2 = 4 
+# ele = (numero1**numero2)
+# print(ele)
+
+# numero =5 
+# cubo = (numero**3)
+# print(cubo)
+
+# lista = [8,12,15]
+# soma = sum(lista)
+# media = int(soma/3)
+# print(media)
+
+# numero = 10 
+# numero1 = 2 
+# mult = (numero - numero1)
+# resultado = int(mult**5)
+# print(resultado)
+
+# numero1 = 15 
+# numero2 = 6 
+# mult = (numero1 * numero2)
+# resultado = int(mult//5)
+# print(resultado)
+
+# numero1 = 27 
+# numero2 = 3 
+# div = (numero1 // numero2) 
+# resultado =int(div + 5)
+# print(resultado)
+
+
+# numero1 = 9
+# numero2 = 3
+# mult = (numero1 * numero2)
+# resultado = (mult**2)
+# print(resultado)
+
+lista = [9,5,20]
+soma = sum(lista)
+media = int(soma//3) 
+print(media)
